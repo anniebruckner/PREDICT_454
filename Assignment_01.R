444f6dd7afbd1aee8c189452e2d49f2636efbd4e
anniebruckner <annie.bruckner@gmail.com>
2017-01-05 19:18:49 -0600

    test

--- Assignment_01.R
+++ Assignment_01.R
@@ -14,3 +14,4 @@ colnames(wine) <- c("Class", "Alcohol", "Malic_Acid", "Ash", "Ash_Alcalinity",
                     "Magnesium", "Total_Phenols", "Flavanoids", 
                     "Nonflavanoid_Phenols", "Proanthocyanins", 
                     "Color_Intensity", "Hue", "OD280_OD315", "Proline")
+# test
