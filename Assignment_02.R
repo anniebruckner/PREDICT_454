cd79209ba9111200b461230ca38e1032f5948ec8
anniebruckner <annie.bruckner@gmail.com>
2017-01-23 21:09:19 -0600

    DQC + tree

--- Assignment_02.R
+++ Assignment_02.R
@@ -59,4 +59,32 @@ names(data)
 str(data) # all int or numeric
 summary(data) # no missing data
 
+# Convert to numeric
+data$price <- as.numeric(data$price)
 
+# Convert to factor
+data$color <- as.factor(data$color)
+data$clarity <- as.factor(data$clarity)
+
+summary(data$color) # 1 to 9
+summary(data$clarity) # 2 to 10
+
+# Code factor levels (per two_months_salary.pdf)
+levels(data$color) <- c("D", "E", "F", "G", "H", "I", "J", "K", "L")
+levels(data$clarity) <- c("IF", "VVS1", "VVS2", "VS1", "VS2", "SI1", "SI2", "I1", "I2")
+
+# Fix data values so they have no spaces
+summary(data$cut)
+levels(data$cut) <- c("Ideal", "Not_Ideal")
+
+summary(data$store)
+levels(data$store) <- c("Ashford", "Ausmans", "Blue_Nile", "Chalmers", 
+                      "Danford", "Fred_Meyer", "Goodmans", "Kay", 
+                      "R_Holland", "Riddles", "University", "Zales")
+
+#######################################################
+# EDA
+#######################################################
+
+# Creat tree plot
+fancyRpartPlot(rpart(data$price ~ ., data = data), sub = "")
